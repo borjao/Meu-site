553c5f6b314d2b8ca89f5e5d4150f09cef8855db
João Víctor <john.bsb11@gmail.com>
2023-02-16 11:36:41 -0300

    att2

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   //$contact->smtp = array(
     //'host' => 'smtp.gmail.com',
     //'username' => 'john.bsb11@gmail.com',
-    //'password' => '9803Joao!',
+    //'password' => '',
     //'port' => '587'
   //);*
 
